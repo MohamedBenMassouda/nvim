c28ba595cfd2fa216bebadd0d7b14d2d8c8425b8
MohamedBenMassouda <benmassoudamohamed1@gmail.com>
2026-07-22 14:07:05 +0100

    IDK

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -6,8 +6,7 @@ return {
 		vim.o.timeoutlen = 300
 	end,
 	opts = {
-		-- your configuration comes here
-		-- or leave it empty to use the default settings
-		-- refer to the configuration section below
+		{ "<leader>q", group = "Session" },
+		{ "<leader>r", group = "Rename/Refactor" },
 	},
 }
