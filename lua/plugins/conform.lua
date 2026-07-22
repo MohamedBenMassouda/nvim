8be34b4370574014537432d0e6cdfa34e6aaed41
MohamedBenMassouda <benmassoudamohamed1@gmail.com>
2026-07-22 14:07:00 +0100

    Edits

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,34 +1,16 @@
 return {
   "stevearc/conform.nvim",
-  config = function()
-    require("conform").setup({
-      formatters_by_ft = {
-        lua = { "stylua" },
-        -- Conform will run multiple formatters sequentially
-        python = { "isort", "ruff_format" },
-        -- Use a sub-list to run only the first available formatter
-        javascript = { { "prettierd", "prettier" } },
-        typescript = { { "prettierd", "prettier" } },
-        typescriptreact = { { "prettierd", "prettier" } },
-        -- cs = { "csharpier" },
-      },
-      format_on_save = {
-        -- These options will be passed to conform.format()
-        timeout_ms = 500,
-        lsp_format = "fallback",
-      },
-      formatters = {
-        csharpier = {
-          command = "dotnet-csharpier",
-        }
-      }
-    })
-
-    vim.api.nvim_create_autocmd("BufWritePre", {
-      pattern = "*",
-      callback = function(args)
-        require("conform").format({ bufnr = args.buf })
-      end,
-    })
-  end,
+  opts = {
+    formatters_by_ft = {
+      lua = { "stylua" },
+      python = { "isort", "ruff_format" },
+      javascript = { "prettierd", "prettier", stop_after_first = true },
+      typescript = { "prettierd", "prettier", stop_after_first = true },
+      typescriptreact = { "prettierd", "prettier", stop_after_first = true },
+    },
+    format_on_save = {
+      timeout_ms = 1000, -- Roslyn/csharpier can be slow on first run
+      lsp_format = "fallback",
+    }
+  },
 }
